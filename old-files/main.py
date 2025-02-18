b55426a300fd575922a415b5f28d49109f7c2ec8
musaburake <musaburak.erdihan@gmail.com>
2025-02-18 21:06:48 +0300

    clean-up

--- old-files/main.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-main_page = Blueprint('main_page', __name__)
-
-@main_page.route('/')
-def main():
-    return render_template('index.html')  # Ana sayfa
